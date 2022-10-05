05cdbdd724aead4a869fff62fbeb5a1f1c739533
Juan Daniel <juandaniellesmes23@gmail.com>
2022-10-05 12:10:40 -0500

    apply rubocop (ruby-styles-guide)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
-class CommentsController < ApplicationController
+# frozen_string_literal: true
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
@@ -14,10 +15,10 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article), status: :see_other
   end
-  
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 end
-  
